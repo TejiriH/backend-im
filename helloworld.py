adfac26340f9cd4080c2ae3c6c8a7341e470c344
Ubuntu <ubuntu@ip-172-31-24-187.eu-north-1.compute.internal>
2025-02-20 19:36:32 +0000

    adjustments

---
+++ helloworld.py
@@ -0,0 +1,9 @@
+# helloworld.py
+def hello_world():
+    print("Hello, World!")
+    # Intentional error: Missing parenthesis in the print statement.
+    print "This will cause a syntax error"
+
+if __name__ == "__main__":
+    hello_world()
+
