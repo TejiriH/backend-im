01c188684621826a1c8e547400ea88228023d865
tejirih <Henryfly09winner@gmail.com>
2025-02-24 21:49:23 +0000

    adjust

--- test.py
+++ test.py
@@ -2,6 +2,15 @@
 def add(a, b):
     return a + b
 
+# def test_add():
+  #  assert add(2, 3) == 5  # This is a pytest test function
+
+
+    # test.py
+
+from helloworld import add
+
 def test_add():
-    assert add(2, 3) == 5  # This is a pytest test function
+    assert add(2, 3) == 6  # This will fail because 2 + 3 is 5, not 6
+
 
